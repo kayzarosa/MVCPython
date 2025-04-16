4ae9f8e8dc85ad4d340744a72a36a231505ae939
kayza <kayzakizy@gmail.com>
2025-04-16 20:12:44 -0300

    config: Adding Pylint to project

---
+++ ex_pylint.py
@@ -0,0 +1,13 @@
+"""Alguma documentaćão sobre o arquivo"""
+
+print("Hello, World!")
+
+
+def minha_funcao():
+    """Alguma documentaćão sobre a função"""
+    print("Estou na minha função")
+
+
+# snake_case -> Variaveis e funćões em Python: my_variable V
+# camelCase -> Não consta: minhaFuncao xxxxxx
+# PascalCase -> Classe em Python
