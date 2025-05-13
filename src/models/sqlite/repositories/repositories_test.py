d65a0963baa6c7f07de760f0b27dd73b3d32ce29
kayza <kayzakizy@gmail.com>
2025-05-13 18:34:06 -0300

    feat: Implementing pets_repository with first test

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -2,7 +2,7 @@ import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
 
-db_connection_handler.connect_to_db()
+# db_connection_handler.connect_to_db()
 
 
 @pytest.mark.skip(reason="interação com o banco")
@@ -10,3 +10,10 @@ def test_list_pets():
     repo = PetsRepository(db_connection_handler)
     response = repo.list_pets()
     print(response)
+
+
+@pytest.mark.skip(reason="interação com o banco")
+def test_delete_pet():
+    name = "belinha"
+    repo = PetsRepository(db_connection_handler)
+    repo.delete_pets(name)
