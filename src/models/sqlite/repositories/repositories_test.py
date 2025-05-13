e07c7ffa97b34c3d0624efee4c308b011738add0
kayza <kayzakizy@gmail.com>
2025-05-13 18:04:34 -0300

    test: repository

---
+++ src/models/sqlite/repositories/repositories_test.py
@@ -0,0 +1,12 @@
+import pytest
+from src.models.sqlite.settings.connection import db_connection_handler
+from .pets_repository import PetsRepository
+
+db_connection_handler.connect_to_db()
+
+
+@pytest.mark.skip(reason="interação com o banco")
+def test_list_pets():
+    repo = PetsRepository(db_connection_handler)
+    response = repo.list_pets()
+    print(response)
